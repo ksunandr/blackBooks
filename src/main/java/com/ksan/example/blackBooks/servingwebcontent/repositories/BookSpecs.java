7eeb564a6580b9120615fde32fe405cbf36a5331
ksun <41472942+ksunandr@users.noreply.github.com>
2020-08-20 01:01:41 +0300

    specs afterYear rightYear rightYear

--- src/main/java/com/ksan/example/blackBooks/servingwebcontent/repositories/BookSpecs.java
+++ src/main/java/com/ksan/example/blackBooks/servingwebcontent/repositories/BookSpecs.java
@@ -8,21 +8,27 @@ import javax.persistence.criteria.*;
 public final class BookSpecs {
 
 
-    public static Specification<Book> byName(final String name) {
-        return new Specification<Book>() {
-            @Override
-            public Predicate toPredicate(Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-                return name == null ? null : builder.equal(root.get("name"), name);
-            }
-        };
+    public static Specification<Book> nameIs(final String name) {
+        return (Specification<Book>) (root, query, builder) -> name == null ? builder.conjunction() : builder.equal(root.get("name"), name);
     }
 
-    public static Specification<Book> byAuthorName(final String authorName) {
-        return new Specification<Book>() {
-            @Override
-            public Predicate toPredicate(Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-                return authorName == null ? null: builder.equal(root.join("authors", JoinType.INNER).get("surname"), authorName);
-            }
-        };
+    public static Specification<Book> authorNameIs(final String authorName) {
+        return (Specification<Book>) (root, query, builder) -> authorName == null ?  builder.conjunction() : builder.equal(root.join("authors", JoinType.INNER).get("surname"), authorName);
+    }
+
+    public static Specification<Book> afterYear(final Integer year) {
+        return (Specification<Book>) (root, query, builder) -> year == null ? builder.conjunction() : builder.ge(root.get("publicationYear"), year);
+    }
+
+    public static Specification<Book> rightYear(final Integer year) {
+        return (Specification<Book>) (root, query, builder) -> year == null ? builder.conjunction() : builder.le(root.get("publicationYear"), year);
+    }
+
+    public static Specification<Book> inStock(final Boolean inStock) {
+        return (Specification<Book>) (root, query, builder) -> inStock == null ?
+                builder.conjunction() :
+                (inStock ?
+                        builder.gt(root.get("inStock"), 0) :
+                        builder.le(root.get("inStock"), 0));
     }
 }
